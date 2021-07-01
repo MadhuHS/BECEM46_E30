026f8b6c470a73ee82bd54a2b16c7afccbebef2d
Madhu <Madhu@192.168.1.4>
2021-07-01 14:27:24 +0530

    Encapsulation started

---
+++ ClassWork/src/com/jspiders/encapsulation/Mainclass.java
@@ -0,0 +1,12 @@
+package com.jspiders.encapsulation;
+
+public class Mainclass {
+
+	public static void main(String[] args)
+	{
+    System.out.println("Program starts...");
+    
+    System.out.println("Program ends..");
+	}
+
+}
