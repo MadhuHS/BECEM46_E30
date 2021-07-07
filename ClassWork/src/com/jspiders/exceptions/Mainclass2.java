989f7acbc44ae023299217269ab4a7ed5e5c1a0d
Madhu <Madhu@192.168.1.11>
2021-07-07 14:50:30 +0530

    Exceptions updated

--- ClassWork/src/com/jspiders/exceptions/Mainclass2.java
+++ ClassWork/src/com/jspiders/exceptions/Mainclass2.java
@@ -1,31 +1,30 @@
 package com.jspiders.exceptions;
 
-public class Mainclass
+public class Mainclass2
 {
-	public static void test(int num1, int num2)
+	public static void test(int num1, int num2) 
 	{
 		System.out.println("test() starts..");
-		System.out.println("num1 : "+num1 +" num2 : "+num2);
-		
-		if(num2 != 0)
+		System.out.println("num1 : " + num1 + " num2 : " + num2);
+
+		try
 		{
 		  int res = num1 / num2;
 		  System.out.println(res);
 		}
-		else
+		catch(ArithmeticException ae)
 		{
-		   System.out.println("cannot divide by "+num2);
+			System.out.println("ArithmeticException Handled");
 		}
-      
+
 		System.out.println("test() ends...");
 		System.out.println();
 	}
 
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
 		System.out.println("Program starts..");
-		test(50,2);
-		test(50,0);
+		test(50, 2);
+		test(50, 0);
 		System.out.println("Program ends...");
 	}
 }
---
+++ ClassWork/src/com/jspiders/exceptions/Mainclass4.java
@@ -0,0 +1,38 @@
+package com.jspiders.exceptions;
+
+public class Mainclass4
+{
+	public static void test(int num1, int num2) 
+	{
+		System.out.println("test() starts..");
+		System.out.println("num1 : " + num1 + " num2 : " + num2);
+		
+		int[] a1 = {20,40,80,100};
+
+		try
+		{
+		  int res = num1 / num2;
+		  System.out.println(res);
+		  System.out.println("array element : "+a1[res]);
+		}
+		catch(Exception exp)
+		{
+			exp.printStackTrace();
+		}
+		finally
+		{
+			System.out.println("finally block executed");
+		}
+
+		System.out.println("test() ends...");
+		System.out.println();
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Program starts..");
+		test(50,0);
+		test(50,2);
+		test(50,25);
+		System.out.println("Program ends...");
+	}
+}
