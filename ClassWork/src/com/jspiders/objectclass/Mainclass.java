189ed5590a06756aec46c678e7d9aedf46bcef11
Madhu <Madhu@192.168.1.11>
2021-07-09 15:37:09 +0530

    Object class updated

---
+++ ClassWork/src/com/jspiders/objectclass/Mainclass.java
@@ -0,0 +1,33 @@
+package com.jspiders.objectclass;
+
+public class Mainclass
+{
+	public static void main(String[] args)
+	{
+      System.out.println("Program starts..");
+      
+      Object obj1 = new Object();
+      Object obj2 = new Object();
+      Object obj3 = obj1;
+      
+      int h1 = obj1.hashCode();
+      
+      System.out.println(h1);
+      System.out.println(obj2.hashCode());
+      
+      
+      String s1 = obj1.toString();
+      System.out.println(s1);
+      
+      System.out.println(obj2);//println--obj2.toString()
+      
+     boolean b1 = obj1.equals(obj2);
+     
+     System.out.println(b1);
+     
+     System.out.println(obj1.equals(obj3));
+      
+      
+      System.out.println("Program ends..");
+	}
+}
