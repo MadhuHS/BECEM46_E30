32b0aa8fce748281edd0453bb6a363e12df8806c
Madhu <Madhu@192.168.1.11>
2021-07-21 15:00:02 +0530

    JDBC insert method added

--- ClassWork/src/com/jspiders/jdbc/Mainclass.java
+++ ClassWork/src/com/jspiders/jdbc/Mainclass.java
@@ -2,42 +2,82 @@ package com.jspiders.jdbc;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 
-public class Mainclass {
-	public static void main(String[] args) {
-		System.out.println("Program starts...");
-
+public class Mainclass 
+{
+	static Connection dbCon= null;
+	
+	public static void openConnection() throws SQLException
+	{
 		String dburl = "jdbc:mysql://localhost:3306/AppOneDB";
 		String dbUsername = "root";
 		String dbPasswrod = "root";
-		Connection dbCon= null;
+		
+	       //register and load the driver
+			    dbCon = DriverManager.getConnection(dburl,dbUsername,dbPasswrod);
+				System.out.println("Connection opend successfully...");
+				
+	}
+	
+	public static void closeConnection() throws SQLException
+	{
+		dbCon.close();
+		System.out.println("Connection closed successfully...");
+	}
+	
+	
+	
+	public static void insertUser(User us)
+	{
+		//Create --> Insert
+		 String insertUserQuery = "insert into PracticeDB.User values(?,?,?,?)";
+		//Statement stmt = dbCon.createStatement();
+		
+		PreparedStatement prstmt;
+		try {
+			prstmt = dbCon.prepareStatement(insertUserQuery);
+			prstmt.setString(1, us.getUid());
+			prstmt.setString(2, us.getName());
+			prstmt.setString(3, us.getEmail());
+			prstmt.setString(4, us.getMob());
+			
+			
+			prstmt.executeUpdate();
+			System.out.println("Record inserted to DB");
+		}
+		catch (SQLException e)
+		{
+			System.out.println("OOPS!! User already exists in the App");
+		}	
+	}
+	
+	public static void main(String[] args)
+	{
+		System.out.println("Program starts...");
 
 		try
 		{
-			       //register and load the driver
-		    dbCon = DriverManager.getConnection(dburl,dbUsername,dbPasswrod);
-			System.out.println("Connection opend successfully...");
+			openConnection();
 			
-		} 
+			User us1 = new User();
+			us1.setUid("US002");
+			us1.setName("ABD");
+			us1.setEmail("ABD@rcb.com");
+			us1.setMob("776717192");
+			
+			insertUser(us1);
+			
+			closeConnection();
+		}
 		catch (SQLException e)
 		{
 			e.printStackTrace();
+		}
+
 		
-		}
-		finally
-		{
-			try
-			{
-				dbCon.close();
-				System.out.println("Connection closed successfully...");
-			} 
-			catch (SQLException e)
-			{
-				e.printStackTrace();
-			}
-			
-		}
 
 		System.out.println("Program ends...");
 	}
---
+++ ClassWork/src/com/jspiders/jdbc/User.java
@@ -0,0 +1,42 @@
+package com.jspiders.jdbc;
+
+//Entity
+public class User {
+	private String uid;
+	private String name;
+	private String email;
+	private String mob;
+
+	public String getUid() {
+		return uid;
+	}
+
+	public void setUid(String uid) {
+		this.uid = uid;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getMob() {
+		return mob;
+	}
+
+	public void setMob(String mob) {
+		this.mob = mob;
+	}
+
+}
